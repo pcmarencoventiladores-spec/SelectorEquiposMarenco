a48ec3dbbd5d0a5bfaac04ed3eb6410dc8cdf88c
pcmarencoventiladores-spec <pc.marencoventiladores@gmail.com>
2026-09-01 12:21:29 -0600

    Add main.jsx to render SelectorVentilacion component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SelectorVentilacion from "./SelectorVentilacion.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SelectorVentilacion />
+  </React.StrictMode>
+);
